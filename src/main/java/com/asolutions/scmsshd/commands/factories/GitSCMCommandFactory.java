7896a5f27db0acc7c3a029a0e9030c91ad383685
Oliver Ferrigni <oferrigni@oliver-ferrignis-macbook-pro.local>
2010-09-13 22:35:30 +0800

    Removed unneeded class that contributed only constructor data.  Leveraged existing factory instead.
    Oliver

--- src/main/java/com/asolutions/scmsshd/commands/factories/GitSCMCommandFactory.java
+++ src/main/java/com/asolutions/scmsshd/commands/factories/GitSCMCommandFactory.java
@@ -5,7 +5,8 @@ import java.util.Properties;
 import org.apache.sshd.server.CommandFactory.Command;
 
 import com.asolutions.scmsshd.commands.FilteredCommand;
-import com.asolutions.scmsshd.commands.git.GitSCMCommand;
+import com.asolutions.scmsshd.commands.git.GitSCMCommandHandler;
+import com.asolutions.scmsshd.commands.git.SCMCommand;
 import com.asolutions.scmsshd.converters.path.IPathToProjectNameConverter;
 import com.asolutions.scmsshd.sshd.IProjectAuthorizer;
 
@@ -14,10 +15,16 @@ public class GitSCMCommandFactory implements ISCMCommandFactory {
 	public static final String REPOSITORY_BASE = "repositoryBase";
 
 	public Command create(FilteredCommand filteredCommand,
-			IProjectAuthorizer projectAuthenticator,
+			IProjectAuthorizer projectAuthorizer,
 			IPathToProjectNameConverter pathToProjectNameConverter,
 			Properties configuration) {
-		return new GitSCMCommand(filteredCommand, projectAuthenticator, pathToProjectNameConverter, configuration);
+		SCMCommand retVal = new SCMCommand();
+		retVal.setFilteredCommand(filteredCommand);
+		retVal.setProjectAuthorizer(projectAuthorizer);
+		retVal.setSCMCommandHandler(new GitSCMCommandHandler());
+		retVal.setPathToProjectNameConverter(pathToProjectNameConverter);
+		retVal.setConfiguration(configuration);
+		return retVal;
 	}
 
 }
